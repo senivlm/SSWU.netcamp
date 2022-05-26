7d571fc4e848487fcb8493620ca93acf82c5dcc9
qix1k <105869351+qix1k@users.noreply.github.com>
2022-05-26 14:46:35 +0200

    Add files via upload

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Products;
+
+Product Salt = new Product("Salt", 50, 1);
+Product Meat = new Product("Meat", 100, 2);
+
+Buy List1 = new();
+
+List1.AddProduct(Meat, 3);
+
+List1.AddProduct(Salt, 4);
+
+//List1.RemoveProduct(1, 4);
+
+Check.ShowCheck(List1);
+
+
