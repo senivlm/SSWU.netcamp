b9a72961e943a3c4d9e4dc70f80d41ec7b313818
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-14 12:39:47 +0300

    Update Program.cs

--- Homework6/TextReader/Program.cs
+++ Homework6/TextReader/Program.cs
@@ -4,5 +4,6 @@ Text.TextReader Reader = new Text.TextReader("File1.txt");
 
 Reader.TextReadFromFile();
 Reader.TextOutput();
+// Ім'я файлу треба було передати як параметр
 Reader.PrintSentencesToFile();
 Reader.SizeWords();
